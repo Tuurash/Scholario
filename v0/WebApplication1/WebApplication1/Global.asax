2f0de628f7031546a5e9ea66a7562d45fbddeb03
Tuurash <turasmimoo@gmail.com>
2020-03-19 15:31:20 +0600

    Repository Added

---
+++ v0/WebApplication1/WebApplication1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Scholar.MvcApplication" Language="C#" %>
---
+++ v0/WebApplication1/WebApplication1/Repository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Scholar.Models;
+using System.Threading.Tasks;
+
+
+namespace WebApplication1.Repository
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity Get(int id);
+        void Insert(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(int id);
+    }
+}
